2e0de4cd51a78ac59aed8505d5e3459373532255
milos018 <milos018@gmail.com>
2021-04-08 09:15:14 +0200

    finished basic layout header+footer react+vue L5

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,19 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Container>
-          <h1>Welcome to Proshop</h1>
-        </Container>
-      </main>
-      <Footer />
-    </>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to Proshop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <app-header></app-header>
+  <main class="py-3">
+    <div class="container">
+      <h1>Welcome to ProShop</h1>
+      <!-- <router-view /> -->
+    </div>
+  </main>
+  <app-footer></app-footer>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import AppHeader from "./shared/UI/AppHeader";
+import AppFooter from "./shared/UI/AppFooter";
+export default {
+  components: { AppHeader, AppFooter },
+  setup() {
+    return {};
   }
+};
+</script>
+
+<style lang="scss">
+main {
+  min-height: 80vh;
 }
 </style>
